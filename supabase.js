ee74135353d7e458ec7238e10324ce31baf6514b
Engineer-Coder <43961119+Engineer-Coder@users.noreply.github.com>
2026-06-02 08:35:39 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://hxjojtgvxzgeouthylwa.supabase.co';
+const supabaseAnonKey = 'sb_publishable_e7ESpEOIZS1hmOTpckYewg_HLOBaQwe';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
